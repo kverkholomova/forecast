10d6710e89b9ed45c0e7947fd65349441937d2d1
Verkholomova Karina <verholomova_karina@ukr.net>
2022-09-07 10:31:24 +0200

    Files added

---
+++ lib/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        // title: Text(widget.title),
+      ),
+      body: const Center(
+
+
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}

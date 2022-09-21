be04f41ba224c8b41eb5d26595843fc44a067dc9
Karina <verholomova_karina@ukr.net>
2022-09-21 09:30:39 +0200

    Added logo and splash screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:forecast/screens/main_page.dart';
 import 'package:forecast/screens/home_page.dart';
@@ -16,8 +17,13 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ),
-      home:  MainPage(),
-    );
+    ),
+    home: AnimatedSplashScreen(
+    splash: 'assets/forecast_logo.png',
+    splashIconSize: 180,
+    nextScreen: MainPage(),
+    splashTransition: SplashTransition.sizeTransition,
+    duration: 3000,
+    ));
   }
 }
--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -36,38 +36,43 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        bottomNavigationBar: TabBar(
-          controller: controllerTab,
-          unselectedLabelColor: Colors.black45,
-          labelColor: Colors.indigoAccent.withOpacity(0.6),
-          indicatorColor: Colors.indigoAccent,
-          // onTap: (number) {
-          //   switch (number) {
-          //     case 0:
-          //       print("Selected Index2: " + _controller.index.toString());
-          //       loading=true;
-          //       // loading_today=true;
-          //       break;
-          //     case 1:
-          //       print("Selected Index3: " + _controller.index.toString());
-          //       // loading=true;
-          //       loading_today=true;
-          //       break;
-          //     default:
-          //       loading=true;
-          //       loading_today=true;
-          //   }
-          // },
-          tabs: [
-            Tab(icon: Text("Today", style: GoogleFonts.roboto(
-              fontSize: 18,
-              // color: Colors.indigoAccent.withOpacity(0.7),
-            ),)),
-            Tab(icon: Text("5Days", style: GoogleFonts.roboto(
-              fontSize: 18,
-              // color: Colors.indigoAccent.withOpacity(0.7),
-            ),)),
-          ],
+
+        bottomNavigationBar: Container(
+          color: Colors.white,
+          child: TabBar(
+            // overlayColor: Colors.white,
+            controller: controllerTab,
+            unselectedLabelColor: Colors.black45,
+            labelColor: Colors.indigoAccent.withOpacity(0.6),
+            indicatorColor: Colors.indigoAccent,
+            // onTap: (number) {
+            //   switch (number) {
+            //     case 0:
+            //       print("Selected Index2: " + _controller.index.toString());
+            //       loading=true;
+            //       // loading_today=true;
+            //       break;
+            //     case 1:
+            //       print("Selected Index3: " + _controller.index.toString());
+            //       // loading=true;
+            //       loading_today=true;
+            //       break;
+            //     default:
+            //       loading=true;
+            //       loading_today=true;
+            //   }
+            // },
+            tabs: [
+              Tab(icon: Text("Today", style: GoogleFonts.roboto(
+                fontSize: 18,
+                // color: Colors.indigoAccent.withOpacity(0.7),
+              ),)),
+              Tab(icon: Text("5Days", style: GoogleFonts.roboto(
+                fontSize: 18,
+                // color: Colors.indigoAccent.withOpacity(0.7),
+              ),)),
+            ],
+          ),
         ),
 
         body: TabBarView(

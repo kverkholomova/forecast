20506a8d5e08b532bf6221cbccf9446e1d1c9d73
Karina <verholomova_karina@ukr.net>
2022-10-13 11:35:41 +0200

    Working on tests

--- test/weather_week_api_test.dart
+++ test/weather_week_api_test.dart
@@ -1,14 +1,11 @@
 
 
-import 'dart:convert';
 import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:forecast/api/weather_week_api.dart';
-import 'package:forecast/models/weather_week_model.dart';
 import 'package:forecast/screens/today_forecast.dart';
 import 'package:forecast/utils/location_functionality.dart';
-import 'package:http/http.dart';
 import 'package:mockito/annotations.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
@@ -27,9 +24,8 @@ void main(){
       when(client
           .get(Uri.parse(city!=""?'http://api.openweathermap.org/data/2.5/forecast?q=$city&cnt=40&appid=43ec70748cae1130be4146090de59761&units=metric':'http://api.openweathermap.org/data/2.5/forecast?lat=${currentLocationData.latitude}&lon=${currentLocationData.longitude}&cnt=40&appid=43ec70748cae1130be4146090de59761&units=metric')))
           .thenAnswer((_) async =>
-          http.Response(null!, 200));
-
-      expect(await fetchWeather(client), null!);
+          http.Response(" ", 200));
+      expect(await fetchWeather(client), " ");
     });
 
     test('throws an exception if the http call completes with an error', () async{
